a56641f0950b7916d3ea59a44b35e807220b4e4f
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2025-02-26 17:53:57 -0600

    3.9 Backend de la Agenda Telefónica, paso 9, 3.10 Backend de la Agenda Telefónica, paso 10, 3.11 Agenda Telefónica Full Stack

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors'); //middleware que perminte la solicitud de informac
 const bodyParser = require('body-parser'); 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static('dist')); //middleware necesario para leer archivos static
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
